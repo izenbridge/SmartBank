62bcffacbeb26960907c8d8ae2c0e9f44013e0c9
AM002AG <AM002AG@IN-5S9KG32.pbi.global.pvt>
2015-08-12 17:01:07 +0530

    Test modified

--- src/test/java/com/smartbank/atm/controller/LogoutControllerTest.java
+++ src/test/java/com/smartbank/atm/controller/LogoutControllerTest.java
@@ -5,8 +5,10 @@ import static org.mockito.Mockito.when;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import junit.framework.Assert;
+//import junit.framework.Assert;
 
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
